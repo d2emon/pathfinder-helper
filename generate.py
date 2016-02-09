ea49a49e839c7978174d11a078c6d73a190b62df
Dmitry Kutsenko <d2emonium@gmail.com>
2016-02-09 09:30:58 +0300

    Using commandline

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@
 
 
 import logging
+import sys
 import yaml
 import character
 import abilities
@@ -10,53 +11,32 @@ import charsheet
 import race
 
 
-def defineAbility():
-    count = len(character.STATS)
-    logging.info("Determine Ability Scores")
-    with open("test.yml", "r") as f:
-        data = yaml.load(f)
-    print(data)
-    pool = {
-        "STR": 3,
-        "CON": 4,
-        "INT": 5,
-        "WIS": 6,
-    }
-    chars = [
-        character.Char(
-            name="Stnd",
-            rollMethod=abilities.ROLL_STANDARD,
-            raceId=race.RACE_DWARF,
-        ),
-        character.Char(
-            name="Clas",
-            rollMethod=abilities.ROLL_CLASSIC,
-            raceId=race.RACE_ELF,
-        ),
-        character.Char(
-            name="Hero",
-            rollMethod=abilities.ROLL_HEROIC,
-            raceId=race.RACE_GNOME,
-        ),
-        character.Char(
-            name="Pool",
-            rollPool=pool,
-            raceId=race.RACE_HALFELF,
-        ),
-        character.Char(
-            name="Pool",
-            rollPool=pool,
-            raceId=race.RACE_HALFLING,
-        ),
-        character.Char(**data),
-    ]
+def defineAbility(count=1, method=abilities.ROLL_STANDARD, chars=[], pool=None):
+    count -= len(chars)
+    rooster = []
 
+    logging.info("Determine Ability Scores")
+    logging.debug("Loading %s", chars)
     for c in chars:
+        rooster.append(character.Char(**c))
+    logging.debug("%d character(s) left", count)
+    logging.debug(method)
+
+    i = race.RACE_UNKNOWN
+    rooster += [character.Char(
+        name="Character %d" % (c),
+        rollMethod=method,
+        raceId=i + c + 1,
+        dicePool=pool
+    ) for c in range(count)]
+
+    logging.debug(rooster)
+    for c in rooster:
         print("-" * 80)
         charsheet.showChar(c)
     print("-" * 80)
 
-    return chars
+    return rooster
 
 
 def pickRace():
@@ -79,8 +59,49 @@ def finishDetails():
     print("Finishing Details")
 
 
-def main():
-    chars = defineAbility()
+def helpMessage():
+    # TODO: Help message
+    print("Help!")
+
+
+def main(argv):
+    import getopt
+
+    try:
+        opts, args = getopt.getopt(argv, "hdl:c:r:f:", ["help", "debug", "logfile=", "count", "roll=", "file=", "logformat"])
+    except(getopt.GetoptError):
+        opts = ("-h", "")
+
+    logconfig = {"format": "%(asctime)s: [%(levelname)s]:\t%(message)s"}
+    count = 1
+    method = abilities.ROLL_STANDARD
+    chars = []
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            helpMessage()
+        elif opt in ("-d", "--debug"):
+            logconfig["level"] = logging.DEBUG
+        elif opt in ("-l", "--logfile"):
+            logconfig["filename"] = arg
+        elif opt in ("--logformat"):
+            logconfig["format"] = arg
+        elif opt in ("-c", "--count"):
+            count = int(arg)
+        elif opt in ("-r", "--roll"):
+            methods = {
+                "standard": abilities.ROLL_STANDARD,
+                "classic": abilities.ROLL_CLASSIC,
+                "heroic": abilities.ROLL_HEROIC,
+            }
+            method = methods[arg]
+        elif opt in ("-f", "--file"):
+            with open(arg, "r") as f:
+                chars = yaml.load(f)
+
+    logging.basicConfig(**logconfig)
+    logging.info("Starting generator")
+
+    chars = defineAbility(count=count, method=method, chars=chars)
     pickRace()
     pickClass()
     pickSkills()
@@ -89,9 +110,4 @@ def main():
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(levelname)s:%(message)s"
-    )
-
-    main()
+    main(sys.argv[1:])

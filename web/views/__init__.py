660a440334f33f74e5eb54278f01b0891d953eb8
Dmitry Kutsenko <d2emonium@gmail.com>
2017-02-27 01:19:52 +0300

    Add & del

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -15,11 +15,46 @@ def index():
     return redirect(url_for("rpg_list"))
 
 
+@app.route("/rpg")
+def rpg_list():
+    rpg_id = 0
+    return render_template("rpg.html", games=games, selected=games[rpg_id])
+
+
+@app.route("/rpg/add")
+def rpg_add():
+    return redirect(url_for("char_list"))
+
+
+@app.route("/rpg/del")
+def rpg_del():
+    return redirect(url_for("char_list"))
+
+
+@app.route("/rpg/<int:rpg_id>")
+def select_rpg(rpg_id):
+    if rpg_id <= 0:
+        return redirect(url_for("rpg_list"))
+
+    rpg = games[rpg_id - 1]
+    return redirect(url_for("char_list"))
+
+
 @app.route("/char")
 def char_list():
     g.pc = pc
     selected = pc[0]
     return redirect(url_for("charsheet", char_id=selected.id))
+
+
+@app.route("/char/add")
+def char_add():
+    return redirect(url_for("char_list"))
+
+
+@app.route("/char/del")
+def char_del():
+    return redirect(url_for("char_list"))
 
 
 @app.route("/char/<int:char_id>")
@@ -30,19 +65,3 @@ def charsheet(char_id):
     g.pc = pc
     g.selected = pc[char_id - 1]
     return render_template("char.html")
-
-
-@app.route("/rpg")
-def rpg_list():
-    rpg_id = 0
-
-    return render_template("rpg.html", games=games, selected=games[rpg_id])
-
-
-@app.route("/rpg/<int:rpg_id>")
-def select_rpg(rpg_id):
-    if rpg_id <= 0:
-        return redirect(url_for("rpg_list"))
-
-    rpg = games[rpg_id - 1]
-    return redirect(url_for("char_list"))

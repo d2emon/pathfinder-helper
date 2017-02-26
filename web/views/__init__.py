eff900ebe44d881e9670f3d8996ca5a8da58a49f
Dmitry Kutsenko <d2emonium@gmail.com>
2017-02-27 00:49:10 +0300

    RPG list

--- web/views/__init__.py
+++ web/views/__init__.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 from web import app
-from flask import g, render_template
+from flask import g, render_template, redirect
 from flask.helpers import url_for
+
 
 # from web.views.char import character, new_character, edit_character
 # from web.views.fraction import fraction, new_fraction, edit_fraction
@@ -42,6 +43,12 @@ class PlayerCharacter:
         return url_for("charsheet", char_id=self.id)
 
 
+class RPG:
+    def __init__(self, id=0, name="RPG"):
+        self.id = id
+        self.name = name
+        
+
 pc = [
     PlayerCharacter(1, "Char1"),
     PlayerCharacter(2, "Char2"),
@@ -52,15 +59,49 @@ pc = [
     ]
 
 
+games = [
+    RPG(1, "ADnD 2nd edition"),   
+    RPG(2, "ADnD 3rd edition"),   
+    RPG(3, "ADnD 3.5 edition"),   
+    RPG(4, "ADnD-Wyrms"),   
+    ]
+
+
 @app.route("/")
 def index():
+    return redirect(url_for("rpg_list"))
+
+
+@app.route("/char")
+def char_list():
     global pc
     g.pc = pc
-    return render_template("index.html", selected=g.pc[0], prev_id=1, next_id=2)
+    selected = pc[0]
+    return redirect(url_for("charsheet", char_id=selected.id))
 
 
 @app.route("/char/<int:char_id>")
 def charsheet(char_id):
+    if char_id <= 0:
+        return redirect(url_for("char_list"))
+
     global pc
     g.pc = pc
-    return render_template("char.html", selected=g.pc[char_id-1], prev_id=char_id-1, next_id=char_id+1)
+    g.selected = pc[char_id - 1]
+    return render_template("char.html")
+
+
+@app.route("/rpg")
+def rpg_list():
+    global games
+    return render_template("rpg.html", games=games)
+
+
+@app.route("/rpg/<int:rpg_id>")
+def select_rpg(rpg_id):
+    if rpg_id <= 0:
+        return redirect(url_for("rpg_list"))
+
+    global games
+    rpg = games[rpg_id - 1]
+    return redirect(url_for("char_list"))

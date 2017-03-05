5353a18686d59e0d083bd4143cb19b3f81571f99
Dmitry Kutsenko <d2emonium@gmail.com>
2017-03-06 01:47:59 +0300

    Session editing

--- web/views/session.py
+++ web/views/session.py
@@ -17,7 +17,7 @@ def session_list():
     if campaign is None:
         return redirect(url_for('campaign_list'))
 
-    sessions = GameSession.query.filter(GameSession.campaign_id==campaign.id).all() 
+    sessions = GameSession.query.filter(GameSession.campaign_id == campaign.id).all()
     return render_template("session/list.html", sessions=sessions)
 
 
@@ -36,10 +36,10 @@ def session_edit(session_id=0):
         gs.real_date = form.real_date.data
         gs.world_date = form.world_date.data
         gs.description = form.description.data
-        
+
         db_session.add(gs)
         db_session.commit()
-        
+
         flash("Сессия {} успешно добавлена".format(gs))
         return redirect(url_for('session_list'))
 
@@ -47,23 +47,26 @@ def session_edit(session_id=0):
     form.real_date.data = gs.real_date
     form.world_date.data = gs.real_date
     form.description.data = gs.description
-    return render_template("session/edit.html", form=form, session_id=gs.id)
+
+    if gs.id:
+        session_id = gs.id
+    return render_template("session/edit.html", form=form, session_id=session_id)
 
 
 @app.route("/session/del/<int:session_id>")
 def session_del(session_id):
-    gs = GameSession.query.get(session_id) 
+    gs = GameSession.query.get(session_id)
     flash("Сессия {} успешно удалена".format(gs))
     db_session.delete(gs)
     db_session.commit()
-        
+
     return redirect(url_for("session_list"))
 
 
 @app.route("/session/<int:session_id>")
 def session_show(session_id):
-    gs = GameSession.query.get(session_id) 
+    gs = GameSession.query.get(session_id)
     session["session"] = gs
-    
+
     return redirect(url_for("char_list"))
     # return render_template("campaigns.html", campaigns=campaigns, selected=games[rpg_id])
